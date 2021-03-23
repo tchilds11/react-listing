31ce5c71c3a91e8b73cb7911a715dc79689be494
Terrence Childs <tchilds1108@gmail.com>
2021-03-22 23:14:10 -0400

    nav layout complete

--- src/App.js
+++ src/App.js
@@ -24,20 +24,22 @@ const handleListingList = (status) => {
   <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
     <ReactBootStrap.Nav className="mr-auto">
-      <ReactBootStrap.Nav.Link href="#features">Features</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="#pricing">Pricing</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Divider />
-        <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
+      <ReactBootStrap.Nav.Link href="#features">Account Information</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link href="#pricing">Saved Listings</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.NavDropdown title="Quick Links" id="collasible-nav-dropdown">
+        <ReactBootStrap.NavDropdown.Item href="#action/3.1">Java</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#action/3.2">Javascript</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#action/3.3">Redux</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#action/3.4">React</ReactBootStrap.NavDropdown.Item>
+		<ReactBootStrap.NavDropdown.Item href="#action/3.5">Python</ReactBootStrap.NavDropdown.Item>
+		<ReactBootStrap.NavDropdown.Item href="#action/3.6">Node</ReactBootStrap.NavDropdown.Item>
+		<ReactBootStrap.NavDropdown.Item href="#action/3.7">Express</ReactBootStrap.NavDropdown.Item>
       </ReactBootStrap.NavDropdown>
     </ReactBootStrap.Nav>
     <ReactBootStrap.Nav>
-      <ReactBootStrap.Nav.Link href="#deets">More deets</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link href="#deets">Contact Us</ReactBootStrap.Nav.Link>
       <ReactBootStrap.Nav.Link eventKey={2} href="#memes">
-        Dank memes
+        About The Developer
       </ReactBootStrap.Nav.Link>
     </ReactBootStrap.Nav>
   </ReactBootStrap.Navbar.Collapse>
